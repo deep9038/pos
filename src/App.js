9811167affc3272ed43e07bc008282b5cdd612d7
deep <deep22sarkar@gmail.com>
2022-09-28 18:18:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Body from './component/Body/Body';
+import Foot from './component/Foot/Foot';
+import Hading from './component/Hadding/Hading';
+import ItemModal from './component/ItemModal/ItemModal';
 
 function App() {
+  const [openItrmModal,setOpenItemModal]=useState(false)
+  const [addItem,setAddItem]=useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Hading setAddItem={setAddItem} />
+    <Body OpenMod={setOpenItemModal} setOpenItemModal={setOpenItemModal} addItem={addItem} />
+    <Foot/>
+    {openItrmModal?<ItemModal setOpenItemModal={setOpenItemModal} />:""}
+    </>
   );
 }
 
